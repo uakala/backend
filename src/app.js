039fe19140254107389e077da56da12ed390fb42
Arnau Garcia <arnau@evpassport.com>
2024-05-04 22:22:37 +0200

    feat: incremented max size of body

--- src/app.js
+++ src/app.js
@@ -18,6 +18,9 @@ app.use((req, res, next) => {
     next();
 });
 
+// increment max size of request body
+app.use(bodyParser.json({ limit: '50mb' }));
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
