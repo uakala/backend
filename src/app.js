4d02b3f87c81e632e48727456cb7ac7c03e29197
Arnau Garcia <arnau@evpassport.com>
2024-05-04 20:39:15 +0200

    feat: disabled CORS

--- src/app.js
+++ src/app.js
@@ -9,6 +9,15 @@ dotenv.config();
 
 const app = express();
 
+// Disable CORS globally
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Credentials', 'true')
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+});
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
